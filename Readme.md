db26bc6deeff1071f291dfdc70c770f429449518
HungND <danghung24031992@gmail.com>
2017-09-30 09:14:02 +0700

    first commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+To build app release, please fix these files in node_modules
+- update version package buffer trong react-native-jwt fix loi Buffer.alloc is not a function
+- edit file react-native-jwt/node-modules/pbkdf2/index.js, comment line 3->5# CameraU
---
+++ index.android.js
@@ -0,0 +1,71 @@
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  * @flow
+//  */
+
+'use strict';
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View
+} from 'react-native';
+import Camera from 'react-native-camera';
+
+class CameraU extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Camera
+          barcodeScannerEnabled={true}
+          onBarCodeRead={this.barcodeReceived.bind(this)}
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+          style={styles.preview}
+          aspect={Camera.constants.Aspect.fill}>
+          <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+        </Camera>
+      </View>
+    );
+  }
+
+  barcodeReceived(e) {
+    console.log(e);
+  }
+
+  takePicture() {
+    const options = {};
+    //options.location = ...
+    this.camera.capture({metadata: options})
+      .then((data) => console.log(data))
+      .catch(err => console.error(err));
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
+  }
+});
+
+
+AppRegistry.registerComponent('CameraU', () => CameraU);
